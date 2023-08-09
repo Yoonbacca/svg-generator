7686c7b1b1be4aa29f943e498b3b59be1529ade8
YoonJae Kim <yoonjaekim3@gmail.com>
2023-08-09 13:35:00 -0500

    Created tests for logo.js

--- test/logo.test.js
+++ test/logo.test.js
@@ -1,2 +1,30 @@
-const Shapes = require('../shapes.js');
-
+const Logo = require('../lib/logo.js');
+const colors = require('../lib/colors.json');
+const colorKeyArray = Object.keys(colors);
+console.log(colorKeyArray);
+// describe('Logo', () => {
+//     describe('Text Length', () => {
+//         it('should throw an error if given more than 3 characters', () => {
+//             const badString = 'ab12';
+//             const characters = new Logo()
+//         });
+//     });
+//     describe('Text Color', () => {
+//         it('should throw an error if given more than 3 characters', () => {
+//             const badString = 'ab12';
+//             const characters = new Logo()
+//         });
+//     });
+//     describe('Shape Type', () => {
+//         it('should throw an error if given more than 3 characters', () => {
+//             const badString = 'ab12';
+//             const characters = new Logo()
+//         });
+//     });
+//     describe('Shape Color', () => {
+//         it('should throw an error if given more than 3 characters', () => {
+//             const badString = 'ab12';
+//             const characters = new Logo()
+//         });
+//     });
+// });
