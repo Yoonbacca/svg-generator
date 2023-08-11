225a365e240da7606bea460425bbd046713555cc
YoonJae Kim <yoonjaekim3@gmail.com>
2023-08-11 18:43:27 -0500

    Created basic class and constructor for Logo

--- test/logo.test.js
+++ test/logo.test.js
@@ -1,30 +1,52 @@
 const Logo = require('../lib/logo.js');
 const colors = require('../lib/colors.json');
 const colorKeyArray = Object.keys(colors);
-console.log(colorKeyArray);
-// describe('Logo', () => {
-//     describe('Text Length', () => {
-//         it('should throw an error if given more than 3 characters', () => {
-//             const badString = 'ab12';
-//             const characters = new Logo()
-//         });
-//     });
-//     describe('Text Color', () => {
-//         it('should throw an error if given more than 3 characters', () => {
-//             const badString = 'ab12';
-//             const characters = new Logo()
-//         });
-//     });
-//     describe('Shape Type', () => {
-//         it('should throw an error if given more than 3 characters', () => {
-//             const badString = 'ab12';
-//             const characters = new Logo()
-//         });
-//     });
-//     describe('Shape Color', () => {
-//         it('should throw an error if given more than 3 characters', () => {
-//             const badString = 'ab12';
-//             const characters = new Logo()
-//         });
-//     });
-// });
+
+describe('Logo', () => {
+    describe('Text Length', () => {
+        it('should throw an error if given more than 3 characters', () => {
+            const badString = 'ab12';
+            const result = new Logo(badString, 'green', 'triangle', 'blue');
+
+            expect(result).toEqual(false);
+        });
+    });
+    describe('Text Color', () => {
+        it('should throw an error if given an invalid color keyword', () => {
+            const badString = 'lightestgreen';
+            const result = new Logo('abc', badString, 'triangle', 'blue');
+
+            expect(result).toEqual(false);
+        });
+
+        it('should throw an error if given an invalid hexadecimal', () => {
+            const badString = 'lightestgreen';
+            const result = new Logo('abc', badString, 'triangle', 'blue');
+
+            expect(result).toEqual(false);
+        });
+    });
+    describe('Shape Type', () => {
+        it('should throw an error if given an invalid shape', () => {
+            const badString = 'hexagon';
+            const result = new Logo('abc', 'green', badString, 'blue');
+
+            expect(result).toEqual(false);
+        });
+    });
+    describe('Shape Color', () => {
+        it('should throw an error if given an invalid color keyword', () => {
+            const badString = 'lightestgreen';
+            const result = new Logo('abc', 'green', 'triangle', badString);
+
+            expect(result).toEqual(false);
+        });
+
+        it('should throw an error if given an invalid hexadecimal', () => {
+            const badString = 'lightestgreen';
+            const result = new Logo('abc', 'green', 'triangle', badString);
+
+            expect(result).toEqual(false);
+        });
+    });
+});
